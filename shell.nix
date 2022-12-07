395c8f3b57a58c1e324bcbb9d121dce8c2de7af3
Michael Wisely <michael.wisely@gravwell.io>
2022-12-07 13:19:23 -0700

    Don't allow nixpkgs to be overridden

--- shell.nix
+++ shell.nix
@@ -1,15 +1,12 @@
 let
   # use a specific (although arbitrarily chosen) version of the Nix package collection
-  default_pkgs = fetchTarball {
+  pkgs = import (fetchTarball {
     url =
       "https://github.com/NixOS/nixpkgs/archive/c3616ce8370f4a00e6b62af80fdaace308c13b68.tar.gz";
     # the sha256 makes sure that the downloaded archive really is what it was when this
     # file was written
     sha256 = "0cbf5y66zllj63ndlcng5jlc5fhpp7ph1ribgi989xmdplf0h1r1";
-  };
-  # function header: we take one argument "pkgs" with a default defined above
-in { pkgs ? import default_pkgs { } }:
-let
+  }) { };
   pythonBundle = pkgs.python310.withPackages (ps:
     with ps; [
       sphinx
